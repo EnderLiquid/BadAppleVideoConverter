c98bbffa441d69407441966575c2fe68545fa48b
EnderLiquid <3546817839@qq.com>
2026-03-21 12:09:14 +0800

    refactor:更改项目名称

--- src/main/java/top/enderliquid/BadAppleVideoConverter.java
+++ src/main/java/top/enderliquid/BadAppleVideoConverter.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BadAppleConverter {
+public class BadAppleVideoConverter {
 
     public static final int MAX_WIDTH = 8192;
     public static final int MAX_HEIGHT = 8192;
